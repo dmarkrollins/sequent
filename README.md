52c009c152ee2230f296c4d6a17b21c1955fcb0e
Dave Rollins <drollins@sdcsol.com>
2017-12-17 15:14:34 -0500

    Archive version number and css tweaks

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ PRs are welcome keeping in mind:
 - It's about the discussion not the tool - anything that promotes better team discussion is welcome.
 - Team members should be able to participate from whereever they using whatever device is available so cross browser compatibility on different display formats is really important.
 
+Start using Sequent - https://sequent.herokuapps.com
+
 ## Development Info
 
 You need to install Meteor - currently this project is on v1.6
--- server/prompts.js
+++ server/prompts.js
@@ -9,7 +9,16 @@ const RetroPrompts = {
         "Compliment someone",
         "Invent a feature our product needs",
         "Who else should we talk too?",
-        "It would be amazing if..."
+        "It would be amazing if...",
+        "What's blocking us?",
+        "Timing issues",
+        "Ideas for keeping our momentum",
+        "How can we improve team mechanics?",
+        "How could we market ourselves better?",
+        "Identify gaps worth discussing",
+        "What do we still need to discover?",
+        "Stake holder alignment concerns",
+        "What have we succeeded at so far?"
     ],
 
     getRandomPromptSet() {

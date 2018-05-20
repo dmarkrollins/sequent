2fd94bb4056fc8a61ade7a7974cfb51dd7671a1e
Dave Rollins <drollins@sdcsol.com>
2018-05-20 09:06:43 -0400

    Code Cleanup

--- client/routes.js
+++ client/routes.js
@@ -4,11 +4,10 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout'
 import { Retros, RetroActions, Settings } from '../lib/sequent'
 
 FlowRouter.route('/', {
-    action: function() {
+    action: function () {
         if (Meteor.userId()) {
             FlowRouter.go('/retro/board')
-        }
-        else {
+        } else {
             BlazeLayout.render('layout', { content: 'start' });
         }
     },
@@ -16,91 +15,91 @@ FlowRouter.route('/', {
 });
 
 FlowRouter.route('/new', {
-    action: function() {
-        BlazeLayout.render('layout', {content: 'newTeam'});
+    action: function () {
+        BlazeLayout.render('layout', { content: 'newTeam' });
     },
     name: 'new-team'
 });
 
 FlowRouter.route('/versions', {
-    subscriptions: function(params) {
-        this.register('retros', Meteor.subscribe('active-retros'))  
+    subscriptions: function (params) {
+        this.register('retros', Meteor.subscribe('active-retros'))
         this.register('actions', Meteor.subscribe('open-actions'))
-      },
-      action: function() {
-        BlazeLayout.render('retroLayout', {content: 'sequentVersions'});
+    },
+    action: function () {
+        BlazeLayout.render('retroLayout', { content: 'sequentVersions' });
     },
     name: 'versions'
-    
+
 })
 
-RetroRouter = FlowRouter.group({
+var RetroRouter = FlowRouter.group({ // eslint-disable-line
     prefix: '/retro',
     name: 'admin',
-    triggersEnter: [function(context, redirect) {
-        if(!Meteor.userId()) {
+    triggersEnter: [function (context, redirect) {
+        if (!Meteor.userId()) {
             FlowRouter.go('/')
         }
     }]
-  });
+});
 
 RetroRouter.route('/board', {
-    subscriptions: function(params) {
-      this.register('retros', Meteor.subscribe('active-retros'))  
-      this.register('actions', Meteor.subscribe('open-actions'))
-      this.register('settings', Meteor.subscribe('settings'))
+    subscriptions: function (params) {
+        this.register('retros', Meteor.subscribe('active-retros'))
+        this.register('actions', Meteor.subscribe('open-actions'))
+        this.register('settings', Meteor.subscribe('settings'))
     },
-    action: function() {
-        BlazeLayout.render('retroLayout', {content: 'retroBoard'});
+    action: function () {
+        BlazeLayout.render('retroLayout', { content: 'retroBoard' });
     },
     name: 'retro-board'
 })
 
 RetroRouter.route('/actions', {
-    subscriptions: function(params) {
-      this.register('retros', Meteor.subscribe('active-retros'))  
-      this.register('actions', Meteor.subscribe('open-actions'))
-      this.register('settings', Meteor.subscribe('settings'))
+    subscriptions: function (params) {
+        this.register('retros', Meteor.subscribe('active-retros'))
+        this.register('actions', Meteor.subscribe('open-actions'))
+        this.register('settings', Meteor.subscribe('settings'))
     },
-    action: function() {
-        BlazeLayout.render('retroLayout', {content: 'actionsList'});
+    action: function () {
+        BlazeLayout.render('retroLayout', { content: 'actionsList' });
     },
     name: 'actions'
-    
+
 })
 
 RetroRouter.route('/settings', {
-    subscriptions: function(params) {
-        this.register('retros', Meteor.subscribe('active-retros'))  
+    subscriptions: function (params) {
+        this.register('retros', Meteor.subscribe('active-retros'))
         this.register('actions', Meteor.subscribe('open-actions'))
         this.register('backgrounds', Meteor.subscribe('backgrounds'))
         this.register('settings', Meteor.subscribe('settings'))
-      },
-      action: function() {
-        BlazeLayout.render('retroLayout', {content: 'settings'});
+    },
+    action: function () {
+        BlazeLayout.render('retroLayout', { content: 'settings' });
     }
 })
 
 RetroRouter.route('/archives', {
-    subscriptions: function(params) {
-        this.register('retros', Meteor.subscribe('archived-retros'))  
+    subscriptions: function (params) {
+        this.register('retros', Meteor.subscribe('archived-retros'))
         this.register('actions', Meteor.subscribe('open-actions'))
         this.register('settings', Meteor.subscribe('settings'))
-      },
-      action: function() {
-        BlazeLayout.render('retroLayout', {content: 'archive'});
+    },
+    action: function () {
+        BlazeLayout.render('retroLayout', { content: 'archive' });
     },
     name: 'archives'
 })
 
 RetroRouter.route('/archives/:retroId', {
-    subscriptions: function(params) {
-      this.register('retros', Meteor.subscribe('single-archived-retro', params.retroId))  
-      this.register('actions', Meteor.subscribe('open-actions'))
-      this.register('settings', Meteor.subscribe('settings'))
+    subscriptions: function (params) {
+        this.register('retros', Meteor.subscribe('single-archived-retro', params.retroId))
+        this.register('actions', Meteor.subscribe('open-actions'))
+        this.register('settings', Meteor.subscribe('settings'))
     },
-    action: function() {
-        BlazeLayout.render('retroLayout', {content: 'archiveBoard'});
+    action: function () {
+        BlazeLayout.render('retroLayout', { content: 'archiveBoard' });
     },
     name: 'archive-board'
 })
